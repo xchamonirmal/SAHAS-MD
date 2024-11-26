d0dcb373e56133c59831f6445f62a44b2a847950
xchamonirmal <herokubot458@gmail.com>
2024-11-26 10:19:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "0WNXiBYL#APD6eHPWyj5b32bDaQx_ObrdEl38Du0gjiFnqv4HL_E",
+  SESSION_ID: process.env.SESSION_ID || "9XM1WZaC#_MhvFFX6QYPtF66r0HbOv-_O6E0rjRSyZ2VAioBX_U8",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
